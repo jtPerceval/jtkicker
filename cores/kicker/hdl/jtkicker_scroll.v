f0d8f6c8f7e3a3b92025b576d30a921430b0680d
Jose Tejada <jotego@gmail.com>
2021-11-16 17:06:55 +0100

    GFX ROMs interlaced

--- cores/kicker/hdl/jtkicker_scroll.v
+++ cores/kicker/hdl/jtkicker_scroll.v
@@ -90,7 +90,7 @@ always @(posedge clk, posedge rst) begin
         vscr <= 0;
     end else begin
         if( vscr_cs  ) vpos <= cpu_dout;
-        if( hdump[8] ) vscr <= {8{flip}} ^ (vpos+vdump);
+        if( hdump[8] ) vscr <= {8{flip}} ^ (vdump + (vdump>8'o50 ? vpos : 8'h0));
     end
 end
 
