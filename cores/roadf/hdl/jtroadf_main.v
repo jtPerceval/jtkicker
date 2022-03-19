3d6c4e65846bc9cc854f1b7eb00641e21fc9cd1a
Jose Tejada <jotego@gmail.com>
2022-03-19 19:04:19 +0100

    roadf: boots again

--- cores/roadf/hdl/jtroadf_main.v
+++ cores/roadf/hdl/jtroadf_main.v
@@ -129,15 +129,18 @@ function [2:0] rev3( input [2:0] x );
     rev3 = {x[0],x[1],x[2]};
 endfunction
 
+
 always @(posedge clk) begin
+    // Shockingly, if bit 6 for cabinet inputs 1/2 is high, the game won't boot,
+    // however these are regular button inputs in the schematics with pullup resistors
     case( A[1:0] )
         0: cabinet <= { dipsw_c, start_button[1:0], service, coin_input[1:0] };
         1: cabinet <=
             is_hyper ? {1'b1, rev3(joystick2[6:4]), start_button[2], rev3(joystick1[6:4]) } :
-            { 1'b1, joystick1[6:4], joystick1[2], joystick1[3], joystick1[0], joystick1[1]};
+            { 2'b10, joystick1[5:4], joystick1[2], joystick1[3], joystick1[0], joystick1[1]};
         2: cabinet <=
             is_hyper ? {1'b1, rev3(joystick4[6:4]), start_button[3], rev3(joystick3[6:4]) } :
-            { 1'b1, joystick2[6:4], joystick2[2], joystick2[3], joystick2[0], joystick2[1]};
+            { 2'b10, joystick2[5:4], joystick2[2], joystick2[3], joystick2[0], joystick2[1]};
         3: cabinet <= dipsw_a;
     endcase
     cpu_din <= rom_cs  ? rom_data  :
