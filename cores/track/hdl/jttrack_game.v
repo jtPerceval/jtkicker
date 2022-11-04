aaa0639c5f8f1fd8a3737d9c97230c78d9d9b2bf
Samuel Paracuellos <samuel.paracuellos@jotego.es>
2022-11-04 10:42:54 +0100

    Track: FPGA tested

--- cores/track/hdl/jttrack_game.v
+++ cores/track/hdl/jttrack_game.v
@@ -39,22 +39,9 @@ module jttrack_game(
     input   [ 6:0]  joystick4,
     // SDRAM interface
     input           downloading,
-    output          dwnld_busy,
-    output          sdram_req,
-    output  [21:0]  sdram_addr,
-    input   [15:0]  data_read,
-    input           data_dst,
-    input           data_rdy,
-    input           sdram_ack,
+    input    [7:0]  ioctl_dout,
     // ROM LOAD
-    input   [24:0]  ioctl_addr,
-    input   [ 7:0]  ioctl_dout,
     input           ioctl_wr,
-    output reg [21:0] prog_addr,
-    output reg [ 7:0] prog_data,
-    output   [ 1:0] prog_mask,
-    output          prog_we,
-    output          prog_rd,
     // DIP switches
     input   [31:0]  status,     // only bits 31:16 are looked at
     input   [31:0]  dipsw,
@@ -73,8 +60,8 @@ module jttrack_game(
     input   [ 3:0]  gfx_en,
     input   [ 7:0]  debug_bus,
     output  [ 7:0]  debug_view,
-    input           ioctl_ram,
-    output  [ 7:0]  ioctl_din
+     // Memory ports
+    `include "mem_ports.inc"
 );
 
 // SDRAM offsets
@@ -84,57 +71,34 @@ localparam [21:0] SND_START   =  `SND_START,
                   PCM_START   =  `PCM_START;
 localparam [24:0] PROM_START  =  `JTFRAME_PROM_START;
 
-wire        main_cs, main_ok;
-
-wire [12:0] scr_addr;
-wire [13:0] obj_addr;
-wire [31:0] scr_data, obj_data;
-wire        scr_ok, obj_ok, objrom_cs;
-wire [13:0] snd_addr;
-wire [ 7:0] snd_data;
-wire        snd_ok, snd_cs;
-
-wire [ 7:0] main_data;
-wire [15:0] main_addr;
-
 wire [ 7:0] dipsw_a, dipsw_b;
 
 wire        cpu_cen, cpu4_cen;
 wire        cpu_rnw, cpu_irqn, cpu_nmin;
-wire        vram_cs, objram_cs,
-            prom_we, flip;
+wire        vram_cs, objram_cs, flip;
 wire [ 7:0] vram_dout, obj_dout, cpu_dout;
 wire        snd_cen, psg_cen;
-
-// PCM
-wire [15:0] pcm_addr;
-wire [ 7:0] pcm_data;
-wire        pcm_ok;
 
 wire        m2s_irq, m2s_data;
 wire        main_pause;
 
-assign prog_rd    = 0;
-assign dwnld_busy = downloading;
 assign { dipsw_b, dipsw_a } = dipsw[15:0];
 assign dip_flip = ~flip;
 assign main_pause = dip_pause & ~ioctl_ram;
 
-wire [ 7:0] nc, pre_data;
-wire [21:0] pre_addr;
 wire        is_scr, is_obj;
 
 assign is_scr   = ioctl_addr[21:0] >= SCR_START && ioctl_addr[21:0]<OBJ_START;
 assign is_obj   = ioctl_addr[21:0] >= OBJ_START && ioctl_addr[21:0]<PCM_START;
 
 always @(*) begin
-    prog_data = pre_data;
-    prog_addr = pre_addr;
+    post_data = prog_data;
+    post_addr = prog_addr;
     if( is_scr ) begin
-        prog_addr[0] = ~pre_addr[0];
+        post_addr[0] = ~prog_addr[0];
     end
     if( is_obj ) begin
-        prog_addr[4:0] = { pre_addr[2:0], ~pre_addr[4], ~pre_addr[3] };
+        post_addr[4:0] = { prog_addr[2:0], ~prog_addr[4], ~prog_addr[3] };
     end
 end
 
@@ -282,10 +246,10 @@ jttrack_video u_video(
     .scr_data   ( scr_data  ),
     .scr_ok     ( scr_ok    ),
     // Objects
-    .obj_addr   ( obj_addr  ),
-    .obj_data   ( obj_data  ),
+    .obj_addr   ( objrom_addr ),
+    .obj_data   ( objrom_data ),
     .obj_cs     ( objrom_cs ),
-    .obj_ok     ( obj_ok    ),
+    .obj_ok     ( objrom_ok ),
 
     .HS         ( HS        ),
     .VS         ( VS        ),
@@ -298,98 +262,6 @@ jttrack_video u_video(
 
     .gfx_en     ( gfx_en    ),
     .debug_bus  ( debug_bus )
-);
-
-jtframe_dwnld #(.PROM_START(PROM_START),.SWAB(1))
-u_dwnld(
-    .clk            ( clk           ),
-    .downloading    ( downloading   ),
-    .ioctl_addr     ( ioctl_addr    ),
-    .ioctl_dout     ( ioctl_dout    ),
-    .ioctl_wr       ( ioctl_wr      ),
-    .prog_addr      ( pre_addr      ),
-    .prog_data      ( {nc,pre_data} ),
-    .prog_mask      ( prog_mask     ), // active low
-    .prog_we        ( prog_we       ),
-    .prom_we        ( prom_we       ),
-    .sdram_ack      ( sdram_ack     ),
-    .header         (               )
-);
-
-/* verilator tracing_off */
-
-jtframe_rom #(
-    .SLOT0_AW    ( 14              ),
-    .SLOT0_DW    ( 32              ),
-    .SLOT0_OFFSET( SCR_START>>1    ),
-
-    .SLOT1_AW    ( 15              ),
-    .SLOT1_DW    ( 32              ),
-    .SLOT1_OFFSET( OBJ_START>>1    ),
-
-    .SLOT2_AW    ( 16              ),
-    .SLOT2_DW    (  8              ),
-    .SLOT2_OFFSET( PCM_START>>1    ),
-
-    .SLOT6_AW    ( 14              ),
-    .SLOT6_DW    (  8              ),
-    .SLOT6_OFFSET( SND_START>>1    ), // Sound CPU
-
-    .SLOT7_AW    ( 16              ),
-    .SLOT7_DW    (  8              ),
-    .SLOT7_OFFSET(  0              )  // Main
-) u_rom (
-    .rst         ( rst           ),
-    .clk         ( clk           ),
-
-    .slot0_cs    ( LVBL          ),
-    .slot1_cs    ( objrom_cs     ),
-    .slot2_cs    ( 1'b1          ),
-    .slot3_cs    ( 1'b0          ),
-    .slot4_cs    ( 1'b0          ),
-    .slot5_cs    ( 1'b0          ),
-    .slot6_cs    ( snd_cs        ),
-    .slot7_cs    ( main_cs       ),
-    .slot8_cs    ( 1'b0          ),
-
-    .slot0_ok    ( scr_ok        ),
-    .slot1_ok    ( obj_ok        ),
-    .slot2_ok    ( pcm_ok        ),
-    .slot3_ok    (               ),
-    .slot4_ok    (               ),
-    .slot5_ok    (               ),
-    .slot6_ok    ( snd_ok        ),
-    .slot7_ok    ( main_ok       ),
-    .slot8_ok    (               ),
-
-    .slot0_addr  ({scr_addr,1'b0}),
-    .slot1_addr  ({obj_addr,1'b0}),
-    .slot2_addr  ( pcm_addr      ),
-    .slot3_addr  (               ),
-    .slot4_addr  (               ),
-    .slot5_addr  (               ),
-    .slot6_addr  ( snd_addr      ),
-    .slot7_addr  ( main_addr     ),
-    .slot8_addr  (               ),
-
-    .slot0_dout  ( scr_data      ),
-    .slot1_dout  ( obj_data      ),
-    .slot2_dout  ( pcm_data      ),
-    .slot3_dout  (               ),
-    .slot4_dout  (               ),
-    .slot5_dout  (               ),
-    .slot6_dout  ( snd_data      ),
-    .slot7_dout  ( main_data     ),
-    .slot8_dout  (               ),
-
-    // SDRAM interface
-    .sdram_rd    ( sdram_req     ),
-    .sdram_ack   ( sdram_ack     ),
-    .data_dst    ( data_dst      ),
-    .data_rdy    ( data_rdy      ),
-    .downloading ( downloading   ),
-    .sdram_addr  ( sdram_addr    ),
-    .data_read   ( data_read     )
 );
 
 endmodule
